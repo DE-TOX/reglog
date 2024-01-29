9e8e57b62f61ab013d26ac645674d1b684c6f464
DE-TOX <djknight0140@gmail.com>
2024-01-29 12:17:43 +0530

    [DIVYANSH] Modified: Error handlers

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -2,11 +2,7 @@ import { log } from 'winston';
 import User from '../models/user.model';
 import bcrypt from 'bcrypt';
 
-//get all users
-export const getAllUsers = async () => {
-  const data = await User.find();
-  return data;
-};
+
 
 //create new user
 export const newUser = async (body) => {
@@ -20,43 +16,28 @@ export const newUser = async (body) => {
 
 };
 
-//update single user
-export const updateUser = async (_id, body) => {
-  const data = await User.findByIdAndUpdate(
-    {
-      _id
-    },
-    body,
-    {
-      new: true
-    }
-  );
-  return data;
-};
 
-//delete single user
-export const deleteUser = async (id) => {
-  await User.findByIdAndDelete(id);
-  return '';
-};
 
 //get single user
 export const getUser = async (body) => {
 
-  try {
-    const data = await User.findOne({ email_id: body.email_id });
 
-    const ismatch = await bcrypt.compare(body.password,data.password)
+    const data = await User.findOne({ email_id: body.email_id });
+    if(data != null){
+      const ismatch = await bcrypt.compare(body.password,data.password)
 // data && data.password === body.password
-console.log(ismatch);
+
     if (ismatch) {
       // Authentication successful
       return( 'Authentication successful');
     } else {
       // Authentication failed
-      return('Authentication failed');
+      throw new Error('');
     }
-  } catch (error) {
-    return error
-  }
+
+    }else{
+      throw new Error("Invalid Email")
+    }
+    
+  
 };
